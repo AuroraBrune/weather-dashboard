8316cd8c88cf8c491a0a5ec78a9e743d2edd05f1
Aurora Brune <akbrune@hotmail.com>
2020-07-17 18:38:02 -0500

    Pseudocode js for coding.

---
+++ script.js
@@ -0,0 +1,41 @@
+//set document ready function
+$(document).ready(function () {
+
+//dynamically create divs for display dity names in history list
+//write function to display the weather info
+//create button for city input box, used for each city input 
+//set click function for ajax call for that city
+
+//create var for city,
+
+//on click, clear the input box
+//on click add city to history box. prepend the list
+//on click, place call
+    //place the ajax call
+    //create var to hold the response data
+    //create var for temp and convert it to *F
+    //create vars to hold the city data, date, icon for conditions
+    //create vars temp for city, humidity, wind speed, uv rating
+      //append date display
+      //append icon display
+      //append temp display
+      //append humidity display
+
+    //create icon for the uv rating
+    //create vars for each day of five day forecast: date, icon for conditions, temp, humidity
+    //append the data to each day of five day displays
+      
+      //append date display
+      //append icon display
+      //append temp display
+      //append humidity display
+
+//set last city searched to local storage
+//when page loads, get last city searched from local storage and display on page
+
+
+
+    
+
+    
+
